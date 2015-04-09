2c8e17b2f40bdb8f14799229df59193909544a72
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-04-09 07:48:48 +0200

    Add simple test

--- test/index.js
+++ test/index.js
@@ -1,3 +1,8 @@
-var should = require('chai').should(),
+var expect = require('chai').expect,
     hh = require('../index');
 
+describe('#method', function () {
+    it('hh.method is a function', function () {
+        expect(hh.method).a('function');
+    });
+});
